e0b5d1cb874d17ffd041de104d3e03260f28b173
Marilyn <stramberrie972@gmail.com>
2023-11-27 00:25:22 +0100

    Add : - clear test

--- Test.py
+++ Test.py
@@ -1,10 +1,3 @@
-from Option import Options
-from datetime import datetime
-import pandas as pd
 
-df = pd.read_excel(r"C:\Users\Marilyn\Desktop\Optionpython.xlsx")
-# df = df[['Strike','Spot', 'Maturity','Volatility','Type']]
-df['Option Price'] = df[['Strike','Spot', 'Maturity','Volatility','Type']].apply(lambda x: x['Strike']*2 )
-print(df)
 
 
