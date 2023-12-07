2d392e9201c7af16e530d96d0c84a118be143313
Marilyn <stramberrie972@gmail.com>
2023-12-07 11:08:39 +0100

    Add:
    - New classes for Interface.py

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import tkinter as tk
-from Interface import BsmGUI
+from Interface import App
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    bs_gui = BsmGUI(root)
-    root.mainloop()
+    App()
